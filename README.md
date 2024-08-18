7d108be2aa6b73a32a9891984c62b726f1f57985
Ordeeper <pedrordeeper@gmail.com>
2024-08-18 19:14:41 -0300

    refactor: Add initial structure

---
+++ README.md
@@ -0,0 +1,9 @@
+## Contributing
+
+If you encounter any issues or have suggestions for improvements, feel free to
+open an issue or submit a pull request. Contributions are welcome!
+
+## License
+
+This template is available under the [MIT License](LICENSE). Feel free to modify
+and use it to create your own Obsidian plugins.
---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import { Plugin } from "obsidian";
+import { ExampleView, VIEW_TYPE_EXAMPLE } from "./views/ExampleView";
+import "virtual:uno.css";
+
+interface ObsidianNoteConnectionsSettings {
+    mySetting: string;
+}
+
+const DEFAULT_SETTINGS: ObsidianNoteConnectionsSettings = {
+    mySetting: "default",
+};
+
+export default class ObsidianNoteConnections extends Plugin {
+    settings!: ObsidianNoteConnectionsSettings;
+
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData(),
+        );
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    async onload() {
+        await this.loadSettings();
+
+        this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
+
+        this.addRibbonIcon("dice", "Activate view", () => {
+            this.activateView();
+        });
+    }
+
+    onunload() {
+        console.log("unloading plugin");
+    }
+
+    async activateView() {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+        await this.app.workspace.getRightLeaf(false).setViewState({
+            type: VIEW_TYPE_EXAMPLE,
+            active: true,
+        });
+
+        this.app.workspace.revealLeaf(
+            this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0],
+        );
+    }
+}
