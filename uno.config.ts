7b0c40079d6ab80c982deef263ccca60e237b858
Ordeeper <pedrordeeper@gmail.com>
2024-08-21 13:59:49 -0300

    feat(settings): Add Setting Tab and Header

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,6 @@ import extractorSvelte from "@unocss/extractor-svelte";
 import presetUno from "@unocss/preset-uno";
 
 export default defineConfig({
-	presets: [presetUno()],
-	extractors: [extractorSvelte()],
+    presets: [presetUno()],
+    extractors: [extractorSvelte()],
 });
